e4afa41f13e146dd20e4c00f2ce02f254059d459
Sergio Almeida <contato@texasfer.com.br>
2026-08-01 11:51:35 -0300

    Fix: Import redirect in login view

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,5 @@
 from datetime import date
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.db.models import Sum, Count
